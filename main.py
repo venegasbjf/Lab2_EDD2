454fe6a1357d035b735c48dbf894da6f5abdf5ab
Arhenao0206 <127407819+Arhenao0206@users.noreply.github.com>
2023-10-20 00:32:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,38 @@ import pandas as pd
 import folium
 import networkx as nx
 
+# Cargamos los datos
+data = pd.read_csv("flights_final_with_distance.csv")
+
+# Creamos un grafo dirigido con NetworkX
+G = nx.DiGraph()
+
+# Agregamos nodos (aeropuertos) y aristas (rutas) al grafo
+for index, row in data.iterrows():
+    G.add_edge(row['Source Airport Code'], row['Destination Airport Code'], weight=row['Distance (km)'])
+
+# Función para encontrar los 10 caminos más largos desde un vértice dado
+def find_longest_paths(graph, source_node):
+    paths = nx.single_source_dijkstra_path_length(graph, source_node)
+    sorted_paths = sorted(paths.items(), key=lambda x: x[1], reverse=True)
+    return sorted_paths[:10]
+
+# Función para obtener detalles de un aeropuerto dado su código
+def get_airport_details(airport_code):
+    airport_info = data[data['Source Airport Code'] == airport_code]
+    return airport_info.iloc[0]
+
 # Interfaz de Usuario
 while True:
     print("\nMenú:")
     print("1. Mostrar el mapa con el grafo original.")
     print("2. Mostrar la información de los 10 aeropuertos cuyos caminos mínimos desde el vértice dado sean los más largos.")
-    print("3. Mostrar el camino mínimo entre el primer y el segundo vértice sobre el mapa de la interfaz gráfica.")
+    print("3. Camino mínimo entre dos vértices")
     print("4. Salir")
     opcion = input("Selecciona una opción: ")
 
     if opcion == '1':
-        # Cargamos y limpiamos los datos
-        data = pd.read_csv("flights_final_with_distance.csv")
-
-        # Creamos un mapa
+        # Creamos un mapa con Folium
         m = folium.Map(location=[data['Source Airport Latitude'].mean(), data['Source Airport Longitude'].mean()], zoom_start=2)
 
         # Agregamos marcadores para cada aeropuerto
@@ -32,42 +50,19 @@ while True:
         for index, row in data.iterrows():
             airport1 = row['Source Airport Code']
             airport2 = row['Destination Airport Code']
-            distance = row['Distance (km)'] 
+            distance = row['Distance (km)']
 
             coord1 = (row['Source Airport Latitude'], row['Source Airport Longitude'])
             coord2 = (row['Destination Airport Latitude'], row['Destination Airport Longitude'])
-            #folium.PolyLine([coord1, coord2], color='blue', weight=2.5, opacity=1.0, line_cap='round', popup=f"Distance: {row['Distance (km)']} km").add_to(m)
-            folium.PolyLine([coord1, coord2], color='blue', weight=2.5, opacity=1.0, line_cap='round', tooltip=f"Distance: {distance} km").add_to(m)
+            folium.PolyLine([coord1, coord2], color='red', weight=2.5, opacity=1.0, line_cap='round', tooltip=f"Distance: {distance} km").add_to(m)
         
-        print("A continuacaión se esta generando un archivo HTML, una vez este listo, ejecutelo para visualizar el mapa con el grafo.")
+        print("A continuación se está generando un archivo HTML, una vez esté listo, ejecútelo para visualizar el mapa con el grafo.")
         print("Por favor espere...")
         
-        # Guardamos el mapa
+        # Guardamos el mapa como un archivo HTML
         m.save('airports_map_with_graph.html')
-        m
 
-    if opcion == '2':
-        # Cargamos los datos en un DataFrame
-        data = pd.read_csv("flights_final_with_distance.csv")
-
-        # Creamos un grafo dirigido con NetworkX
-        G = nx.DiGraph()
-
-        # Agregamos nodos (aeropuertos) y aristas (rutas) al grafo
-        for index, row in data.iterrows():
-            G.add_edge(row['Source Airport Code'], row['Destination Airport Code'], weight=row['Distance (km)'])
-
-        # Función para encontrar los 10 caminos más largos desde un vértice dado
-        def find_longest_paths(graph, source_node):
-            paths = nx.single_source_dijkstra_path_length(graph, source_node)
-            sorted_paths = sorted(paths.items(), key=lambda x: x[1], reverse=True)
-            return sorted_paths[:10]
-
-        # Función para obtener detalles de un aeropuerto dado su código
-        def get_airport_details(airport_code):
-            airport_info = data[data['Source Airport Code'] == airport_code]
-            return airport_info.iloc[0]
-
+    elif opcion == '2':
         # Ingresamos el código del aeropuerto de inicio
         start_airport_code = input("Ingresa el código del aeropuerto de inicio: ")
 
@@ -82,10 +77,53 @@ while True:
         else:
             print("El código del aeropuerto de inicio no se encuentra en el grafo.")
 
-    #if opcion == '3':
+    elif opcion == '3':
+        start_airport_code = input("Ingresa el código del primer aeropuerto de inicio: ")
+        end_airport_code = input("Ingresa el código del segundo aeropuerto de destino: ")
 
-    if opcion == '4':
+        if start_airport_code in G.nodes and end_airport_code in G.nodes:
+            # Encontrar el camino mínimo entre los dos vértices
+            shortest_path = nx.shortest_path(G, source=start_airport_code, target=end_airport_code, weight='weight')
+
+            # Crear un mapa con Folium
+            m = folium.Map(location=[data['Source Airport Latitude'].mean(), data['Source Airport Longitude'].mean()], zoom_start=2)
+
+            # Agregar marcadores para los aeropuertos en el camino mínimo
+            for airport_code in shortest_path:
+                airport_details = get_airport_details(airport_code)
+                folium.Marker(
+                    location=[airport_details['Source Airport Latitude'], airport_details['Source Airport Longitude']],
+                    popup="Código: {}\nNombre: {}\nCiudad: {}\nPaís: {}  {}\nLatitud:  {}\nLongitud:".format(
+                        airport_details['Source Airport Code'], airport_details['Source Airport Name'],
+                        airport_details['Source Airport City'], airport_details['Source Airport Country'],
+                         airport_details['Source Airport Latitude'], airport_details['Source Airport Longitude'])
+                ).add_to(m)
+
+            # Agregar polilíneas para representar el camino mínimo
+            for i in range(len(shortest_path) - 1):
+                airport1 = shortest_path[i]
+                airport2 = shortest_path[i + 1]
+                distance = G[airport1][airport2]['weight']
+
+                airport1_details = get_airport_details(airport1)
+                airport2_details = get_airport_details(airport2)
+
+                coord1 = (airport1_details['Source Airport Latitude'], airport1_details['Source Airport Longitude'])
+                coord2 = (airport2_details['Source Airport Latitude'], airport2_details['Source Airport Longitude'])
+                folium.PolyLine([coord1, coord2], color='red', weight=2.5, opacity=1.0,
+                                line_cap='round', tooltip=f"Distance: {distance} km").add_to(m)
+
+            print("Se está generando un archivo HTML, una vez esté listo, ejecútelo para visualizar el mapa con el camino mínimo.")
+            print("Por favor espere...")
+
+            # Guardamos el mapa como un archivo HTML
+            m.save('shortest_path_map.html')
+
+        else:
+            print("Uno o ambos códigos de aeropuerto no se encuentran en el grafo.")
+    elif opcion == "4":
         print("Saliendo del programa. ¡Hasta luego!")
         break
+       
            
 
